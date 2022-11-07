eafca997a0f46e8f1432eefb28cb6fb6ecd7ad8c
bukunmitimothy <102929055+bukunmitimothy@users.noreply.github.com>
2022-11-07 13:35:57 +0100

    Create index.py

---
+++ index.py
@@ -0,0 +1,18 @@
+#practing the for_loops
+for _ in "hello, world":
+  print(_)
+
+for a in range(5):
+  for _ in ("welcome"):
+    print(_)
+
+ab = "this is me in python"
+for cd in ab:
+  print(cd)
+#thiis for loop should count the number of worlds in the list bc.
+bc = ["hello", "world", "this", "my", "world"]
+c = 0
+for j in bc:
+  c += 1
+
+print(c)
